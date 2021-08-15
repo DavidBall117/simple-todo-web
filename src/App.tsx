cdb31136f75a567fc6fe0fd5c470707b8b46cdd7
David Ball <davidball117@gmail.com>
2021-08-14 20:28:38 -0400

    Preliminary project structure and configuration.

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.scss';
+import { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Helmet } from 'react-helmet';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import routes from 'Constants/routes';
+import { RouteData } from 'Constants/types';
+
+const App = () => {
+	return (
+		<Router>
+			<Suspense fallback={<div>Loading...</div>}>
+				<Switch>
+					{routes.map((route: RouteData) => (
+						<Route
+							key={route.path}
+							path={route.path}
+							exact={route.exact}
+						>
+							<Helmet>
+								<title>{route.title}</title>
+								<meta name="description" content={route.description} />
+							</Helmet>
+							{route.component}
+						</Route>
+					))}
+				</Switch>
+			</Suspense>
+		</Router>
+	);
+};
 
 export default App;
